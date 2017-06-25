e9872cdee3066b25365ab557b770f65b10ccc1a7
Yuriy Bahur <yuriybahur@gmail.com>
2017-06-25 23:28:32 +0300

    first commit

---
+++ 1st work/js/main.js
@@ -0,0 +1,39 @@
+// Sticky Header
+$(window).scroll(function() {
+
+    if ($(window).scrollTop() > 0) {
+        $('.header').addClass('sticky');
+ }
+ else {
+        $('.header').removeClass('sticky');
+    }
+});
+
+// Mobile Navigation
+$('.mobile-toggle').click(function() {
+    if ($('.header').hasClass('open-nav')) {
+        $('.header').removeClass('open-nav');
+        $('.header').removeClass('sticky-menu');
+        $("ul li").hide();
+    } else {
+        $('.header').addClass('open-nav');
+        $('.header').addClass('sticky-menu');
+         $("ul li").show();
+    }
+});
+
+$('.header li a').click(function() {
+    if ($('.header').hasClass('open-nav')) {
+        $('.nav').removeClass('open-nav');
+        $('.header').removeClass('open-nav');
+    }
+});
+$('nav ul li a').click(function(event) {
+    var id = $(this).attr("href");
+    var offset = 65;
+    var target = $(id).offset().top - offset;
+    $('html, body').animate({
+        scrollTop: target
+    }, 1000);
+    event.preventDefault();
+});
